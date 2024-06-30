b80d21bf05532beafdd4c6986efcef4f1825e155
XSeries5530 <120821571+XSeries5530@users.noreply.github.com>
2024-06-29 23:05:55 -0700

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ const config = {
   challenge: true, // Set to true if you want to enable password protection.
   users: {
     // You can add multiple users by doing username: 'password'.
-    xseries553: "yamotha",
+    xseries553: "placeholder",
     ShadowXR: "pana6531",
     backdoor: "password"
   },
